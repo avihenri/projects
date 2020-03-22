059e65a7daba19734ace180882a3e0012f60d64b
AviHenri <avril.m.henry@gmail.com>
2020-03-22 21:10:03 +0000

    Part finished

--- FilmClub/index.php
+++ FilmClub/index.php
@@ -26,54 +26,36 @@
 
 <body id="page-top">
 
-  <!-- Navigation -->
-  <!-- <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top"> </a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav> -->
-
   <!-- Header -->
   <header class="masthead">
     <div class="container d-flex h-100 align-items-center">
       <div class="mx-auto text-center">
         <h1 class="mx-auto my-0 text-uppercase">FILM CLUB</h1>
         <h2 class="text-white-50 mx-auto mt-2 mb-5"></h2>
-        <a href="#about" class="btn btn-primary js-scroll-trigger">Get Started</a>
+        <a href="#rules" class="btn btn-primary js-scroll-trigger">Get Started</a>
       </div>
     </div>
   </header>
 
   <!-- About Section -->
   <section id="about" class="about-section text-center">
-    <div class="container">
+    <div class="rules-container">
       <div class="row">
-        <div class="col-lg-8 col-md-12 col-sm-12 mx-auto">
-          <h2 class=" mb-4">RULES</h2>
+        <div class="col-lg-10 col-12 mx-auto">
+          <h2 class="mb-4" id="rules">RULES</h2>
           <ul class="noliststyle justify-text rules-list">
-            <li><span class="num-labels btn-primary btn">1</span> <span class="rules">First rule of film club..</span> You must not tell anyone your movie!</li>
-            <li><span class="num-labels">2</span> <span class="rules">Second rule..</span> Each film club should have a theme</li>
-            <li><span class="num-labels">3</span> <span class="rules">Third rule..</span> You must have seen your movie</li>
-            <li><span class="num-labels">4</span> <span class="rules">Fourth rule..</span> Always have a back up</li>
-            <li><span class="num-labels">5</span> <span class="rules">Fifth rule..</span> Each movie should be in one sitting</li>
+            <li><span class="num-labels btn-primary btn">1</span> <span class="rules">First rule of film club..</span> <span class="rule-items">You must not tell anyone your movie!</span></li>
+            <li><span class="num-labels">2</span> <span class="rules">Second rule..</span> <span class="rule-items">Each film club should have a </span></li>
+            <li><span class="num-labels">3</span> <span class="rules">Third rule..</span> <span class="rule-items">You must have seen your movie</span></li>
+            <li><span class="num-labels">4</span> <span class="rules">Fourth rule..</span> <span class="rule-items">Always have a back up</span></li>
+            <li><span class="num-labels">5</span> <span class="rules">Fifth rule..</span> <span class="rule-items">Each movie should be in one sitting</span></li>
           </ul>
+        </div>
+      </div>
+        <!-- Featured Project Row -->
+        <div class="row align-items-center no-gutters mb-4 mb-lg-5" >
+         <div class="" style="width:100%;"> 
+          <div id='popcorn-img'></div>
         </div>
       </div>
     </div>
@@ -82,131 +64,48 @@
   <!-- Projects Section -->
   <section id="projects" class="projects-section bg-light">
     <div class="container">
-
-      <!-- Featured Project Row -->
-      <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-        <div class="col-xl-8 col-lg-7">
-          <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
-        </div>
-        <div class="col-xl-4 col-lg-5">
-          <div class="featured-text text-center text-lg-left">
-            <h4>Shoreline</h4>
-            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Project One Row -->
-      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
-        <div class="col-lg-6">
-          <img class="img-fluid" src="img/demo-image-01.jpg" alt="">
-        </div>
-        <div class="col-lg-6">
-          <div class="bg-black text-center h-100 project">
-            <div class="d-flex h-100">
-              <div class="project-text w-100 my-auto text-center text-lg-left">
-                <h4 class="text-white">Misty</h4>
-                <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
-                <hr class="d-none d-lg-block mb-0 ml-0">
-              </div>
+      <div class="row mb-4">
+        
+      <!-- PEOPLE COUNT -->
+        <div class="col-md-8 margin-auto" id="peopleCountWrapper">
+          <h2 id="club-details">Let&apos;s go..</h2>
+            <label for="inp-people">How many people are in your club?</label>
+            <input type="number" name="people" class="form-control  mr-0 mr-sm-2 mb-3 mb-sm-0" id="inp-people">
+             <div class="center-text">
+              <button id="peopleCount" class="btn btn-primary mt-3">Enter</button>
             </div>
-          </div>
         </div>
-      </div>
 
-      <!-- Project Two Row -->
-      <div class="row justify-content-center no-gutters">
-        <div class="col-lg-6">
-          <img class="img-fluid" src="img/demo-image-02.jpg" alt="">
-        </div>
-        <div class="col-lg-6 order-lg-first">
-          <div class="bg-black text-center h-100 project">
-            <div class="d-flex h-100">
-              <div class="project-text w-100 my-auto text-center text-lg-right">
-                <h4 class="text-white">Mountains</h4>
-                <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
-                <hr class="d-none d-lg-block mb-0 mr-0">
-              </div>
+        <!-- MOVIES -->
+        <div class="col-md-8 margin-auto" id="getMovies" style="display: none;">
+          <h2 id="club-movies">Now the movies..</h2>
+            <label for="inp-movie">Enter movie number <span id="count"></span></label>
+            <input type="movie" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inp-movie" placeholder="Enter your movie..">
+            <div class="center-text">
+              <button id="enterMovie" class="btn btn-primary mt-3">Enter</button>
             </div>
-          </div>
         </div>
-      </div>
 
+        <!-- ALL ENTERED -->
+        <div class="col-md-8 margin-auto center-text" id="allMovies" style="display: none;">
+          <h2 id="club-movies">Great, click below to see your movies in the order they should be watched..</h2>
+            <div class="center-text">
+              <button id="randomiseMovies" class="btn btn-primary mt-3">Show Movies</button>
+            </div>
+        </div>
+
+      </div>
     </div>
   </section>
 
-  <!-- Signup Section -->
-  <!-- <section id="signup" class="signup-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-lg-8 mx-auto text-center">
-
-          <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-          <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
-
-          <form class="form-inline d-flex">
-            <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address...">
-            <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
-          </form>
-
-        </div>
-      </div>
-    </div>
-  </section> -->
 
   <!-- Contact Section -->
   <section class="contact-section bg-black">
     <div class="container">
 
-      <div class="row">
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Address</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">4923 Market Street, Orlando FL</div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-envelope text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Email</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">
-                <a href="#">hello@yourdomain.com</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-mobile-alt text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Phone</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">+1 (555) 902-8832</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="social d-flex justify-content-center">
-        <a href="#" class="mx-2">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#" class="mx-2">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#" class="mx-2">
-          <i class="fab fa-github"></i>
-        </a>
-      </div>
+      <div class="row">       
+        <div id="moviesOrder" style="width:100%;"></div> 
+      </div>  
 
     </div>
   </section>
@@ -214,7 +113,12 @@
   <!-- Footer -->
   <footer class="bg-black small text-center text-white-50">
     <div class="container">
-      Copyright &copy; Your Website 2019
+       &copy; Henri Design Studio <script>document.write(new Date().getFullYear())</script>
+      <div class="social d-flex justify-content-center">
+        <a href="https://github.com/avihenri" class="mb-2">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
     </div>
   </footer>
 
@@ -227,6 +131,54 @@
 
   <!-- Custom scripts for this template -->
   <script src="js/grayscale.js"></script>
+  <script>
+    $(document).ready(function() {
+      let count = 0;
+      let movieArr = [];
+      let countArr = [];
+
+        $('#peopleCount').click(function() {
+          count = $('#inp-people').val();
+          numOfPeeps(count);
+          $('#peopleCountWrapper').hide();
+          $('#getMovies').show();    
+          $('#count').text(countArr[0]);      
+          console.log(countArr);
+        });
+
+        function numOfPeeps(count) {
+          for (i = 1; i <= count; i++) {
+            countArr.push(i);
+          }          
+          return countArr;
+        }
+
+        $('#enterMovie').click(function() {
+          countArr.shift();
+          $('#count').text(countArr[0]); 
+            var movie = $('#inp-movie').val();
+            $('#inp-movie').val('');
+            movieArr.push(movie);
+          if (countArr.length === 0) {
+            $('#getMovies').hide();
+            $('#allMovies').show(); 
+          } 
+        });
+
+      // working progress..
+        $('#randomiseMovies').click(function() {
+          $('#projects').hide();          
+          console.log(movieArr);
+          $.each(movieArr, function(e, val) {
+            $('#moviesOrder').html('<div class="col-md-4 mb-3 mb-md-0"><div class="card py-4 h-100"><div class="card-body text-center"><h4 class="text-uppercase m-0">Movie 1</h4><hr class="my-4"><div class="text-black-50">' + val + '</div></div></div></div>');
+          });          
+        });
+      // ...
+
+
+
+    });
+  </script>
 
 </body>
 
